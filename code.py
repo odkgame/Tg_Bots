24bce291feff48988e021b7b2edd68bee8b9f589
odkgame <odkgame@mail.ru>
2023-04-12 11:23:42 +0300

    try except

--- code.py
+++ code.py
@@ -1,3 +1,33 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram import F
 
-for i in range(0,100):
-    print("hello")
+from PIL import Image
+
+api_token: str = '6139815692:AAG586lZubTtxGsauULpDjyHSWetVCF3b-4'
+
+bot: Bot = Bot(token=api_token)
+dp: Dispatcher = Dispatcher()
+
+
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer('Привет')
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь')
+
+
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Ошибка')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
